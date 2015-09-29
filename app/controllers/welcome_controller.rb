f09828cbf769a8a5e18fc15ca4b4976009c0adfa
Emi Keisuke <emi@swlab.cs.okayama-u.ac.jp>
2015-09-29 22:13:15 +0900

    Add function listing headline in welcome top page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   skip_before_filter :authenticate
 
   def index
+    @headline = ArticlesController.helpers.list_headline(10)
   end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -6,4 +6,8 @@ module ArticlesHelper
       content.split(/\r?\n/)[0, 3].join
     end
   end
+
+  def list_headline(nth)
+    Article.where(promote_headline: true).order("published_on desc").slice(0..nth)
+  end
 end
