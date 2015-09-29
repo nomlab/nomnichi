e0f8e7697fdf9a3fd9abe5291ff6ad9bc14cc2a1
Takuya Okada <pitipitiunsyumikan@gmail.com>
2015-09-29 23:50:34 +0900

    Validate user_id, title, and perma_link of article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ActiveRecord::Base
   belongs_to :user
   has_many :comments
+
+  validates :user_id, presence: true
+  validates :title, presence: true
+  validates :perma_link, presence: true, uniqueness: true
 end
