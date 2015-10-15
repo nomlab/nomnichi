027b44fc48999ff3b6b4cb056f5030eef8b81e33
Takuya Okada <pitipitiunsyumikan@gmail.com>
2015-10-15 11:05:10 +0900

    Add omniauth-related columns to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150929153525) do
+ActiveRecord::Schema.define(version: 20151015015648) do
 
   create_table "articles", force: :cascade do |t|
     t.integer  "user_id"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20150929153525) do
     t.string   "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["ident"], name: "index_users_on_ident", unique: true
