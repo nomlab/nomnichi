1d54074996a677f82382f1b244d58eff67dd08c4
yuko <ike534xx@yahoo.co.jp>
2016-04-07 10:44:24 +0900

    Set default scope to Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :article
+  default_scope { order("created_at desc").visible }
+  scope :visible, -> { User.login? ? Comment.all : Comment.approved }
+  scope :approved, -> { joins(:article).where("approved = ?", true) }
 end
